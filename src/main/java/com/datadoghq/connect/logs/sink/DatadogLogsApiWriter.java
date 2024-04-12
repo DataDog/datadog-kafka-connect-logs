f49031e24aedd01af5e025df620310f349e0e65c
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2024-04-12 14:04:19 -0700

    [AGENT-11265] Update logging (#42)
    * [AGENT-11265] Update logging
    
    Move verbose logs to trace level and update debug logs to be more
    informative.
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- src/main/java/com/datadoghq/connect/logs/sink/DatadogLogsApiWriter.java
+++ src/main/java/com/datadoghq/connect/logs/sink/DatadogLogsApiWriter.java
@@ -68,7 +68,7 @@ public class DatadogLogsApiWriter {
 
     private void sendBatch(String topic) throws IOException {
         JsonArray content = formatBatch(topic);
-        if (content.size() == 0) {
+        if (content.isEmpty()) {
             log.debug("Nothing to send; Skipping the HTTP request.");
             return;
         }
@@ -142,10 +142,11 @@ public class DatadogLogsApiWriter {
         con.setRequestMethod("POST");
         setRequestProperties(con);
 
+        log.trace("Submitting HTTP request to {} with body {}", con.getURL(), requestContent);
         DataOutputStream output = new DataOutputStream(con.getOutputStream());
         output.write(compressedPayload);
         output.close();
-        log.debug("Submitted payload: " + requestContent);
+        log.trace("HTTP request submitted");
 
         // get response
         int status = con.getResponseCode();
@@ -161,12 +162,7 @@ public class DatadogLogsApiWriter {
                     + ", Submitted payload: " + content);
         }
 
-        log.debug("Response code: " + status + ", " + con.getResponseMessage());
-
-        // write the response to the log
-        String response = getOutput(con.getInputStream());
-
-        log.debug("Response content: " + response);
+        log.trace("Received HTTP response {} {} with body {}", status, con.getResponseMessage(), getOutput(con.getInputStream()));
     }
 
     private void setRequestProperties(HttpURLConnection con) {
@@ -196,5 +192,4 @@ public class DatadogLogsApiWriter {
 
         return errorOutput.toString(StandardCharsets.UTF_8.name());
     }
-
 }
